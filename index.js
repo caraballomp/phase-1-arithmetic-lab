a0d3b8df7d062c0101b9e238fccf2ed94cee30ef
Michelle Caraballo <caraballomp@yahoo.com>
2022-02-25 19:16:43 -0600

    commit tree 1f2f005dc16273c35d9be7c8e11168c8d8b701db

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = (num1 * num2);
+
+const random = Math.floor(Math.random() * 10);
+//console.log(Math.floor(random))
+
+const num3 = 19
+const num4 = 5
+
+const mod = (num3 % num4)
+
+// console.log( 44 % 3)
+// console.log( 42 / 3)
+// console.log( 42 % 3)
+//console.log(19 % 5)
+
+const max = Math.max(3, 5, 8, 16, 20);
+console.log(max)
